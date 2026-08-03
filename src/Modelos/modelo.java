7afa82cd2d6cf0bede425557098a3dce91821eaf
Daniel <danielariassolano@gmail.com>
2026-08-03 14:38:59 -0600

    se creo la clase modelo
    se creo la clase modelo con sus metodos sumar, restar, dividir, multiplicar, dividir

---
+++ src/Modelos/modelo.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class modelo {
+    
+     public double sumar(double a, double b) throws ValorInvalidoException {
+        double resultado = a + b;
+        validarValor(resultado);
+        return resultado;  
+    }
+    
+    public double restar(double a, double b) throws ValorInvalidoException {
+        double resultado = a - b;
+        validarValor(resultado);
+        return resultado;
+    }
+    
+    public double multiplicar(double a, double b) throws ValorInvalidoException {
+        double resultado = a * b;
+        validarValor(resultado);
+        return resultado;
+    }
+    
+    public double dividir(double a, double b) throws DivisionPorCeroException, ValorInvalidoException {
+        if (b == 0) {
+            throw new DivisionPorCeroException("No se puede dividir entre cero.");
+        }
+        double resultado = a / b;
+        validarValor(resultado);
+        return resultado;
+    }
+    
+    public void validarValor(double valor) throws ValorInvalidoException {
+        if (Double.isNaN(valor)) {
+            throw new ValorInvalidoException("El resultado no es un número válido.");
+        }
+        if (Double.isInfinite(valor)) {
+            throw new ValorInvalidoException("El resultado es demasiado grande para representarse.");
+        }
+    }
+    
+    public double calcular(double a, double b, String operador)
+            throws DivisionPorCeroException, ValorInvalidoException {
+        if (operador.equals("+")) {
+            return sumar(a, b);
+        } else if (operador.equals("-")) {
+            return restar(a, b);
+        } else if (operador.equals("*")) {
+            return multiplicar(a, b);
+        } else if (operador.equals("/")) {
+            return dividir(a, b);
+        } else {
+            throw new IllegalArgumentException("Operador no reconocido: " + operador);
+        }
+    }
+}
